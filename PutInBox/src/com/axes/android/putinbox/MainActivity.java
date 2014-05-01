17dbe1e9cee86bd4cc1faea9000490cabd27d2bb
mycsoft <mycsoft@qq.com>
2014-05-01 21:02:10 +0800

    列表页加入图片显示.

--- PutInBox/src/com/axes/android/putinbox/MainActivity.java
+++ PutInBox/src/com/axes/android/putinbox/MainActivity.java
@@ -4,84 +4,40 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.widget.Toast;
+
 /**
  * 主应用画面.
+ * 
  * @author MaYichao
- *
+ * 
  */
-public class MainActivity extends ActionBarActivity{
-	
-//	private Button addBtn;
-//	private ListView listView;
+public class MainActivity extends ActionBarActivity {
+
 	/** 最后一次按下Back键的时间 */
 	private long last_back = 0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        addBtn = (Button)findViewById(R.id.add_btn);
-//        addBtn.setOnClickListener(new View.OnClickListener() {
-//			
-//			@Override
-//			public void onClick(View v) {
-//				
-//				App app  = (App) MainActivity.this.getApplication();
-//				app.startAddView();
-//				
-//			}
-//		});
-        
-//        listView = (ListView)findViewById(R.id.listView);
-        
-//        getSupportActionBar().setDisplayUseLogoEnabled(true);
-    }
-    
-//    @Override
-//    protected void onResume() {
-//    	// TODO Auto-generated method stub
-//    	super.onResume();
-//    	Cursor c = Box.queryAllBox(App.openReadableDB(this));
-//    	listView.setAdapter(
-//    			new android.support.v4.widget.SimpleCursorAdapter(this,R.layout.box_list_item, c, new String[]{"name","_id","description"}, new int[]{R.id.name,R.id.id,R.id.desc},0)
-//    			
-//    			);
-//    }
-//
-//
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//    	for ( Fragment frag :getSupportFragmentManager().getFragments()){
-//    		if(frag.hasOptionsMenu()){
-//    			frag.onCreateOptionsMenu(menu, getMenuInflater());
-//    		}
-//    	}
-        return true;
-    }
-//    
-//    @Override
-//    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-//    	if(item.getItemId() == R.id.add){
-//    		App app  = (App) MainActivity.this.getApplication();
-//			app.startAddView();
-//    		return false;
-//    	}else{
-//    		return super.onMenuItemSelected(featureId, item);
-//    		
-//    	}
-//    }
-    
-    @Override
-    	public void onBackPressed() {
-    	long t = System.currentTimeMillis();
-    		if(t - last_back < 2000){
-    			System.exit(0);
-    		}else{
-    			last_back = t;
-    			Toast.makeText(this, "再次点击退出!", Toast.LENGTH_SHORT).show();
-    		}
-    	}
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public void onBackPressed() {
+		long t = System.currentTimeMillis();
+		if (t - last_back < 2000) {
+			System.exit(0);
+		} else {
+			last_back = t;
+			Toast.makeText(this, "再次点击退出!", Toast.LENGTH_SHORT).show();
+		}
+	}
+
 }
--- PutInBox/src/com/axes/android/putinbox/fragment/ListFrgt.java
+++ PutInBox/src/com/axes/android/putinbox/fragment/ListFrgt.java
@@ -14,12 +14,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.axes.android.putinbox.App;
 import com.axes.android.putinbox.R;
 import com.axes.android.putinbox.ViewActivity;
 import com.axes.android.putinbox.dao.Box;
+import com.axes.android.putinbox.task.LoadImageTask;
 
 /**
  * 列表的画面.
@@ -30,15 +32,15 @@ import com.axes.android.putinbox.dao.Box;
 public class ListFrgt extends ListFragment {
 
 	// private Button addBtn;
-	private ListView listView;
+	// private ListView listView;
 	public Integer parentBoxId;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-//		View v = inflater.inflate(R.layout.activity_main, null);
-//		listView = (ListView) v.findViewById(R.id.listView);
-//		return v;
+		// View v = inflater.inflate(R.layout.activity_main, null);
+		// listView = (ListView) v.findViewById(R.id.listView);
+		// return v;
 		return super.onCreateView(inflater, container, savedInstanceState);
 	}
 
@@ -46,25 +48,58 @@ public class ListFrgt extends ListFragment {
 	public void onStart() {
 		super.onStart();
 		setHasOptionsMenu(true);
-//		setMenuVisibility(true);
+		// setMenuVisibility(true);
 		Cursor c = Box.queryAllBox(App.openReadableDB(getActivity()));
-		
-//		listView.setAdapter(
-		setListAdapter(
-				new SimpleCursorAdapter(getActivity(),
+
+		// listView.setAdapter(
+		setListAdapter(new SimpleCursorAdapter(getActivity(),
 				R.layout.box_list_item, c, new String[] { "name", "_id",
 						"description" }, new int[] { R.id.name, R.id.id,
-						R.id.desc }, 0)
+						R.id.desc }, 0) {
+			public View getView(int arg0, View arg1, ViewGroup arg2) {
+				View v = super.getView(arg0, arg1, arg2);
+				 // 显示图片.
+				 // String path =
+				 // ((String)((Map)getItem(arg0)).get("photo_path"));
+				 String path = ((Cursor) getItem(arg0)).getString(4);
+				 if (path != null) {
+				//
+				// File file = new File(path);
+				// if (file.exists()) {
+				ImageView imgV = (ImageView) v.findViewById(R.id.imageView);
+				new LoadImageTask(imgV).execute(path);
+				//
+				// // imgV.setImageURI(Uri.fromFile(new File(path)));
+				//
+				// // 缩小显示
+				// Options opts = new Options();
+				// // 先取得图片的大小.
+				// opts.inJustDecodeBounds = true;
+				// BitmapFactory.decodeFile(path, opts);
+				//
+				// // 计算缩放比例.
+				// opts.inSampleSize = Math.min(imgV.getWidth()
+				// / opts.outWidth, imgV.getHeight()
+				// / opts.outHeight);
+				// opts.inJustDecodeBounds = false;
+				// opts.inPurgeable = true;
+				// Bitmap bmp = BitmapFactory.decodeFile(path, opts);
+				// imgV.setImageBitmap(bmp);
+				// bmp.recycle();
+				// }
+				 }
+				return v;
+			};
+		}
 
 		);
 	}
 
-
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		inflater.inflate(R.menu.main, menu);
+		inflater.inflate(R.menu.list, menu);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (item.getItemId() == R.id.add) {
@@ -76,12 +111,12 @@ public class ListFrgt extends ListFragment {
 
 		}
 	}
-	
+
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		//点击行,显示Box细节画面.
+		// 点击行,显示Box细节画面.
 		Intent i = new Intent(getActivity(), ViewActivity.class);
-		i.putExtra("id", (int)id);
+		i.putExtra("id", (int) id);
 		startActivity(i);
 	}
 
