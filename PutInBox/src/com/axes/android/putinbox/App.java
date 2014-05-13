d6a189db2fa216d459e007afd55989d82e695350
mycsoft <mycsoft@qq.com>
2014-05-13 22:15:41 +0800

    将拍照方法移到App中.

--- PutInBox/src/com/axes/android/putinbox/App.java
+++ PutInBox/src/com/axes/android/putinbox/App.java
@@ -1,9 +1,18 @@
 package com.axes.android.putinbox;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 
 import com.axes.android.putinbox.dao.Dao;
 
@@ -74,6 +83,58 @@ public class App extends Application {
 	public static SQLiteDatabase openReadableDB(Activity act) {
 		App app = getApp(act);
 		return app.dao.getReadableDatabase();
+	}
+	
+	/**
+	 * 打开相机,进行拍照.
+	 */
+	public static File openCamera(Activity context,int requestCode){
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		// Ensure that there's a camera activity to handle the intent
+		if (takePictureIntent.resolveActivity(context.getPackageManager()) != null) {
+			// Create the File where the photo should go
+			File photoFile = null;
+			try {
+				photoFile = createImageFile(context);
+			} catch (IOException ex) {
+				// Error occurred while creating the File
+				// ...
+			}
+			// Continue only if the File was successfully created
+			if (photoFile != null) {
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+						Uri.fromFile(photoFile));
+//				tempPhotoFile = photoFile;
+				context.startActivityForResult(takePictureIntent, requestCode);
+				return photoFile;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * 保存照片.
+	 * 
+	 * @return 照片的文件对象.
+	 * @throws IOException
+	 */
+	private static File createImageFile(Context context) throws IOException {
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		String imageFileName = "JPEG_" + timeStamp + "_";
+		// File storageDir = Environment
+		// .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+		File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+		// File storageDir = Environment.getExternalStorageDirectory();
+		File image = File.createTempFile(imageFileName, /* prefix */
+				".jpg", /* suffix */
+				storageDir /* directory */
+		);
+
+		// Save a file: path for use with ACTION_VIEW intents
+//		mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+		return image;
 	}
 
 }
--- PutInBox/src/com/axes/android/putinbox/EditActivity.java
+++ PutInBox/src/com/axes/android/putinbox/EditActivity.java
@@ -1,16 +1,10 @@
 package com.axes.android.putinbox;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -194,27 +188,27 @@ public class EditActivity extends ActionBarActivity {
 	 */
 	static final int REQUEST_TAKE_PHOTO = 2;
 
-	private void dispatchTakePictureIntent() {
-		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		// Ensure that there's a camera activity to handle the intent
-		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-			// Create the File where the photo should go
-			File photoFile = null;
-			try {
-				photoFile = createImageFile();
-			} catch (IOException ex) {
-				// Error occurred while creating the File
-				// ...
-			}
-			// Continue only if the File was successfully created
-			if (photoFile != null) {
-				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-						Uri.fromFile(photoFile));
-				tempPhotoFile = photoFile;
-				startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-			}
-		}
-	}
+	// private void dispatchTakePictureIntent() {
+	// Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	// // Ensure that there's a camera activity to handle the intent
+	// if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	// // Create the File where the photo should go
+	// File photoFile = null;
+	// try {
+	// photoFile = createImageFile();
+	// } catch (IOException ex) {
+	// // Error occurred while creating the File
+	// // ...
+	// }
+	// // Continue only if the File was successfully created
+	// if (photoFile != null) {
+	// takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+	// Uri.fromFile(photoFile));
+	// tempPhotoFile = photoFile;
+	// startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+	// }
+	// }
+	// }
 
 	/**
 	 * 响应拍照事件.
@@ -222,54 +216,56 @@ public class EditActivity extends ActionBarActivity {
 	 * @param view
 	 */
 	private void photo() {
-		dispatchTakePictureIntent();
+		// dispatchTakePictureIntent();
 		// dispatchTakePictureSmallIntent();
+		tempPhotoFile = App.openCamera(this, REQUEST_IMAGE_CAPTURE);
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-			// 替换照片文件
-			photoFile = tempPhotoFile;
-			// if (loadImageTask != null) {
-			// loadImageTask.cancel(true);
-			// }
-			// loadImageTask = new LoadImageTask(photoView);
-			//
-			// loadImageTask.execute(photoFile.getAbsolutePath());
-			loadImage(photoFile.getAbsolutePath());
-
+			if (tempPhotoFile != null) {
+				// 替换照片文件
+				photoFile = tempPhotoFile;
+				// if (loadImageTask != null) {
+				// loadImageTask.cancel(true);
+				// }
+				// loadImageTask = new LoadImageTask(photoView);
+				//
+				// loadImageTask.execute(photoFile.getAbsolutePath());
+				loadImage(photoFile.getAbsolutePath());
+			}
 		}
 	}
 
 	/**
 	 * 照片路径.
 	 */
-	String mCurrentPhotoPath;
+	// String mCurrentPhotoPath;
 
-	/**
-	 * 保存照片.
-	 * 
-	 * @return 照片的文件对象.
-	 * @throws IOException
-	 */
-	private File createImageFile() throws IOException {
-		// Create an image file name
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
-				.format(new Date());
-		String imageFileName = "JPEG_" + timeStamp + "_";
-		// File storageDir = Environment
-		// .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-		File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-		// File storageDir = Environment.getExternalStorageDirectory();
-		File image = File.createTempFile(imageFileName, /* prefix */
-				".jpg", /* suffix */
-				storageDir /* directory */
-		);
-
-		// Save a file: path for use with ACTION_VIEW intents
-		mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-		return image;
-	}
+	// /**
+	// * 保存照片.
+	// *
+	// * @return 照片的文件对象.
+	// * @throws IOException
+	// */
+	// private File createImageFile() throws IOException {
+	// // Create an image file name
+	// String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+	// .format(new Date());
+	// String imageFileName = "JPEG_" + timeStamp + "_";
+	// // File storageDir = Environment
+	// // .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+	// File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+	// // File storageDir = Environment.getExternalStorageDirectory();
+	// File image = File.createTempFile(imageFileName, /* prefix */
+	// ".jpg", /* suffix */
+	// storageDir /* directory */
+	// );
+	//
+	// // Save a file: path for use with ACTION_VIEW intents
+	// mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+	// return image;
+	// }
 
 }
